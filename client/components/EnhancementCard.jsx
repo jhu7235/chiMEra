ae6ca22db4d63032eb9a096e48d4f81beb0ebcf7
Willy Raedy <wraedy@gmail.com>
2017-07-25 16:00:40 -0500

    fixed a minor select formatting issue

--- client/components/EnhancementCard.jsx
+++ client/components/EnhancementCard.jsx
@@ -6,16 +6,13 @@ export default function EnhancementCard() {
     <div>
       <ul className="collection with-header">
         <li className="collection-header"><h5>Step 2: Pick an Enhancement</h5></li>
-        <li className="collection-item">
-          <div className="input-field col s12">
-            <select>
-              <option value="" disabled selected>Select an Upgrade</option>
-              <option value="1">Wings</option>
-              <option value="2">vacuum</option>
-              <option value="3">taco-lasers</option>
-            </select>
-          </div>
-
+        <li className="collection-item input-field col s12">
+          <select>
+            <option value="" disabled selected>Select an Upgrade</option>
+            <option value="1">Wings</option>
+            <option value="2">vacuum</option>
+            <option value="3">taco-lasers</option>
+          </select>
         </li>
         <li className="collection-item"><ProductCard /></li>
 
