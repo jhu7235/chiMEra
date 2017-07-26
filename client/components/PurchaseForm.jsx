149aa09ac5a7fde7fee9c97bbf74c190d163b55f
Willy Raedy <wraedy@gmail.com>
2017-07-26 17:58:07 -0500

    component: purchase form

--- client/components/PurchaseForm.jsx
+++ client/components/PurchaseForm.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { signup } from '../store';
+import { Row, Input, Button } from 'react-materialize';
 
 const PurchaseForm = (props) => {
-  const { handleSubmit, error } = props;
   return (
+    <div className="container">
+      <h5>Shipping Address</h5>
       <Row>
-          <Input placeholder="Placeholder" s={6} label="First Name" />
-          <Input s={6} label="Last Name" />
-          <Input s={12} label="disabled" defaultValue="I am not editable" disabled />
-          <Input type="password" label="password" s={12} />
-          <Input type="email" label="Email" s={12} />
+        <Input s={12} label="Address" />
+        <Input s={6} label="City" />
+        <Input s={2} label="State" />
+        <Input s={4} label="Zipcode" />
       </Row>
+      <h5>Credit Card</h5>
+      <Row>
+        <Input s={12} label="Number" />
+        <Input s={6} label="Expiration" />
+        <Input s={3} label="CSV" />
+      </Row>
+      <h5>Billing Address</h5>
+      <Row>
+        <Input s={12} label="Address" />
+        <Input s={6} label="City" />
+        <Input s={2} label="State" />
+        <Input s={4} label="Zipcode" />
+      </Row>
+      <Row>
+        <Input s={8} name='terms' type='checkbox' value='terms' label='I agree with the Terms and Conditions' />
+        <Button s={4} waves='light'>Confirm</Button>
+      </Row>
+    </div>
   );
 };
 
