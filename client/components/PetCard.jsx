99268719a2816086b5d75c52171a65202bb286fd
knightofknee <bricarlis@gmail.com>
2017-07-26 11:14:54 -0500

    updated favicon and did cart shell

--- client/components/PetCard.jsx
+++ client/components/PetCard.jsx
@@ -6,13 +6,16 @@ export default function PetCard() {
     <div>
       <ul className="collection with-header">
         <li className="collection-header"><h5>Step 1: Pick a Pet</h5></li>
-        <li className="collection-item input-field col s12">
-          <select>
-            <option value="" disabled selected>Choose a pet</option>
-            <option value="1">catdog</option>
-            <option value="2">horsea</option>
-            <option value="3">birdy</option>
-          </select>
+        <li className="collection-item">
+          <div className="<i></i>nput-field col s12">
+            <select>
+              <option value="" disabled selected>Choose a pet</option>
+              <option value="1">catdog</option>
+              <option value="2">horsea</option>
+              <option value="3">birdy</option>
+            </select>
+          </div>
+
         </li>
         <li className="collection-item"><ProductCard /></li>
 
