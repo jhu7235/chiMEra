831ab218bbd12559cd1ce3b94af2bfc76bdd8bcc
Willy Raedy <wraedy@gmail.com>
2017-07-26 11:09:09 -0500

    initial branch commit

--- client/components/PetCard.jsx
+++ client/components/PetCard.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Collection, CollectionItem, Row, Input } from 'react-materialize';
 import ProductCard from './ProductCard.jsx';
 
-export default function PetCard() {
+export default function PetCard({animals}) {
   return (
-    <div>
+    <Collection header="Step 1: Pick a Pet">
+      <CollectionItem>
+        <Row>
+          <Input s={12} type='select' label="Choose a pet" >
+            {
+              animals.map((animal) => {
+                return <option key={animal.id} value={animal.name}>{animal.name}</option>
+              })
+            }
+          </Input>
+        </Row>
+      </CollectionItem>
+      <CollectionItem>
+        <ProductCard />
+      </CollectionItem>
+    </Collection>
+  );
+}
+
+
+
+{/*<div>
       <ul className="collection with-header">
         <li className="collection-header"><h5>Step 1: Pick a Pet</h5></li>
         <li className="collection-item input-field col s12">
@@ -18,6 +40,4 @@ export default function PetCard() {
 
       </ul>
 
-    </div>
-  );
-}
+    </div>*/}
--- client/components/ProductLab.jsx
+++ client/components/ProductLab.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PetCard from './PetCard.jsx';
 import EnhancementCard from './EnhancementCard.jsx';
 
-export default function ProductLab() {
+function ProductLab({animals}) {
   return (
     <div className="container">
       <div className="row">
-        <div className="col s6"><PetCard /></div>
+        <div className="col s6"><PetCard animals={animals}/></div>
         <div className="col s6"><EnhancementCard /></div>
       </div>
     </div>
   );
 }
+
+const mapState = (state) => {
+  return {
+    animals: state.animals,
+  }
+}
+
+const mapDispatch = null;
+
+export default connect(mapState, mapDispatch)(ProductLab);
