069edd251e50c65d3c59ee1db2a459a4c66af978
Alex Stauter <ajstauter@gmail.com>
2017-07-27 12:59:28 -0500

    navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,47 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Navbar, NavItem, Dropdown, Button } from 'react-materialize';
 import { NavLink } from 'react-router-dom';
 import { logout } from '../store/user';
 
-function isLoggedIn(user) {
-  if (Object.keys(user).length) {
-    return true;
-  }
-  return false;
-}
 
-function Navbar(props) {
-  let user = props.currentUser;
+function Navibar(props) {
+  let user = props.currentUser
+
   return (
     <div>
-      <ul id="login" className="dropdown-content">
-        <li><NavLink to="/login">Login</NavLink></li>
-        <li><NavLink to="/signup">SignUp</NavLink></li>
-      </ul>
-      <ul id="user" className="dropdown-content">
-        <li><NavLink to="/profile">Profile</NavLink></li>
-        <li><NavLink to="/logout">Logout</NavLink></li>
-      </ul>
-      <nav>
-        <div className="nav-wrapper">
-          <a href="/" className="brand-logo">chiMEra</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><NavLink to="/about" >About</NavLink></li>
-            <li><NavLink to="/contact">Contact Us</NavLink></li>
-            <li><NavLink to="/cart" >Cart</NavLink></li>
-            {
-              isLoggedIn(user) ?
-                <li><a className="dropdown-button" href="#!" data-activates="user">{user.firstName} {user.lastName}<i className="material-icons right">arrow_drop_down</i></a></li> :
-                <li><a className="dropdown-button" href="#!" data-activates="login">Login/Sign Up<i className="material-icons right">arrow_drop_down</i></a></li>
-            }
-          </ul>
-        </div>
-      </nav>
+      <Navbar brand="ChiMEra" right>
+        <li><NavLink to="/about">About </NavLink></li>
+        <li><NavLink to="/contact">Contact Us</NavLink></li>
+        <li><NavLink to="/cart">Cart </NavLink></li>
+        <Dropdown trigger={
+          <NavItem>{ user.firstName || 'Login/Signup'}</NavItem>
+        }>
+          {
+            props.isLoggedIn ?
+              <div>
+                <li><NavLink to="/profile">Profile</NavLink></li>
+                <li><NavLink to='/orderHistory'>Order History</NavLink></li>
+                <li><NavLink to='/logout'>Log Out</NavLink></li>
+              </div> :
+              <div>
+                <li><NavLink to="/signup">Sign Up</NavLink></li>
+                <li><NavLink to="/login">Log In</NavLink></li>
+              </div>
+          }
+        </Dropdown>
+      </Navbar>
     </div>
-  );
+  )
 }
 
-const mapStateToProps = state => ({ currentUser: state.user });
+
+const mapStateToProps = state => ({
+  currentUser: state.user,
+  isLoggedIn: !!state.user.id,
+});
 
 const mapDispatchToProps = dispatch => ({
   logout: () => {
@@ -49,4 +47,4 @@ const mapDispatchToProps = dispatch => ({
   }
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+export default connect(mapStateToProps, mapDispatchToProps)(Navibar);
