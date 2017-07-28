34caeb982f4fe85512d32c796ffaf4d185d2e092
Jason Hu <jhu7235@gmail.com>
2017-07-28 13:17:19 -0500

    Revert "Cart routes"

--- server/api/carts.js
+++ server/api/carts.js
@@ -1,69 +1,27 @@
 const router = require('express').Router();
-const { CartItem, User } = require('../db/models');
+const { Cart } = require('../db/models');
 
-router.get('/', (req, res, next) => {
-  const userId = req.user.id;
-  User.findById(userId)
-    .then((user) => {
-      if (!user) next(new Error('User not found'));
-      else return user.getCart();
-    })
-    .then((cart) => {
-      if (!cart) next(new Error('No cart created'));
-      else res.json(cart);
-    })
+router.get('/:userid', (req, res, next) => {
+  const userId = +req.params.userid;
+  Cart.findAll({ where: { userId } })
+    .then(carts => res.json(carts))
     .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+  Cart.create(req.body)
+  .then(cart => res.json(cart))
+  .catch(next);
 });
 
 router.delete('/', (req, res, next) => {
-  const userId = req.user.id;
-  User.findById(userId)
-    .then((user) => {
-      if (!user) next(new Error('User not found'));
-      else return user.getCart();
-    })
-    .then((cart) => {
-      if (!cart) next(new Error('Cart not found'));
-      else return cart.destroy();
-    })
-    .then(() => res.sendStatus(200))
-    .catch(next);
+  Cart.destroy({ where: req.body })
+  .then(() => res.sendStatus(200))
+  .catch(next);
 });
 
-router.post('/item', (req, res, next) => {
-  const userId = req.user.id;
-  const { animalId, enhancementId, quantity, price } = req.body;
-  User.findById(userId)
-    .then(user => user.getCart())
-    .then((cart) => {
-      return CartItem.create({
-        where: { animalId, enhancementId, quantity, price, cartId: cart.id },
-      });
-    })
-    .then(cartItem => res.status(201).json(cartItem))
-    .catch(next);
+router.put('/:cartid', (req, res, next) => {
+  Cart.findById(req.params.cartid)
+  .then(cart => cart.update(req.body))
+  .catch(next);
 });
-
-router.put('/item/:itemId', (req, res, next) => {
-  const { quantity } = req.body;
-  CartItem.findById(req.params.itemId)
-    .then((cartItem) => {
-      if (!cartItem) next(new Error('Cart item not found'));
-      else return cartItem.update({ quantity });
-    })
-    .then(updatedCartItem => res.json(updatedCartItem))
-    .catch(next);
-});
-
-router.delete('/item/:itemId', (req, res, next) => {
-  const itemId = req.params.itemId;
-  CartItem.findById(itemId)
-    .then((cartItem) => {
-      if (!cartItem) next(new Error('Cart item not found'));
-      else return cartItem.destroy;
-    })
-    .then(() => res.status(200).send(itemId))
-    .catch(next);
-});
-
-module.exports = router;
