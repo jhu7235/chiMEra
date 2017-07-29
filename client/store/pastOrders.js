11234794cbf45f8905718df8616650e9b1a5f216
Willy Raedy <wraedy@gmail.com>
2017-07-29 16:23:26 -0500

    past orders reducer and routes

---
+++ client/store/pastOrders.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+/**
+ * ACTION TYPES
+ */
+const GET_PAST_ORDERS = 'GET_PAST_ORDERS';
+const ADD_PAST_ORDER = 'ADD_PAST_ORDER';
+
+/**
+ * ACTION CREATORS
+ */
+const getPastOrders = pastOrders => ({ type: GET_PAST_ORDERS, pastOrders });
+const addPastOrder = pastOrder => ({ type: ADD_PAST_ORDER, pastOrder });
+
+/**
+ * REDUCER
+ */
+
+export default function reducer(pastOrders = [], action) {
+  switch (action.type) {
+    case GET_PAST_ORDERS:
+      return action.pastOrders;
+
+    case ADD_PAST_ORDER:
+      return [action.order, ...pastOrders];
+
+    default:
+      return pastOrders;
+  }
+}
+
+/**
+ * THUNK CREATORS
+ */
+
+export const fetchPastOrders = () => (dispatch) => {
+  axios.get('/api/past-orders')
+    .then(res => res.data)
+    .then((pastOrder) => {
+      dispatch(getPastOrders(pastOrder.pastOrderItems));
+    })
+    .catch(err => console.error('fetching cart unsucessful', err));
+};
+
+export const purchase = (shippingAddress, billingAddress, billingCardInfo) => (dispatch) => {
+  return axios.post('/api/past-orders', { shippingAddress, billingAddress, billingCardInfo })
+    .then(res => res.data)
+    .then((createdPastOrder) => {
+      console.log('Past Order: ', createdPastOrder);
+      dispatch(addPastOrder(createdPastOrder));
+    })
+    .catch(err => console.error('create item unsucessful', err));
+};
