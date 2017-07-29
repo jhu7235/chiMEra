07b41fc5c3be666c1a0ac88fd72a71980f53cf2f
Jason Hu <jhu7235@github.com>
2017-07-29 17:32:41 -0500

    initialize

--- client/components/PreviousCarts.jsx
+++ client/components/PreviousCarts.jsx
@@ -4,10 +4,34 @@ import { connect } from 'react-redux';
 
 function PreviousCarts() {
   return (
-    <div>
-      <h1>Placeholder</h1>
-    </div>
-  )
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Item Name</th>
+          <th>Item Price</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Alvin</td>
+          <td>Eclair</td>
+          <td>$0.87</td>
+        </tr>
+        <tr>
+          <td>Alan</td>
+          <td>Jellybean</td>
+          <td>$3.76</td>
+        </tr>
+        <tr>
+          <td>Jonathan</td>
+          <td>Lollipop</td>
+          <td>$7.00</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 const mapState = (state) => {
