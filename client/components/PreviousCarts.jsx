e18be13ab4409d9ef3a0beaca125cb17c4a8a0ef
Jason Hu <jhu7235@github.com>
2017-07-30 19:00:18 -0500

    update

--- client/components/PreviousCarts.jsx
+++ client/components/PreviousCarts.jsx
@@ -1,62 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { fetchPastOrders } from '../store/pastOrders';
-// #1 get data from server
-// #2 map data to show
 
-class PreviousCarts extends React.Component {
-  constructor() {
-    super();
-  }
 
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
-
-  render() {
-    return (
-      <div className="row center">
-        <div className="col s12 m12 l11">
-          <div className="card">
-            <div className="card-image">
-              <img alt="" src="https://pbs.twimg.com/profile_images/745743929753112587/czNblDjm.jpg" />
-              <span className="card-title">Card Title</span>
-            </div>
-            <div className="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div className="card-action">
-              <a href="#">This is a link</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function PreviousCarts() {
+  return (
+    <div>
+      <h1>Placeholder</h1>
+    </div>
+  )
 }
-// function PreviousCarts() {
-//   return (
-//     <div className="row center">
-//       <div className="col s12 m12 l11">
-//         <div className="card">
-//           <div className="card-image">
-//             <img src="https://pbs.twimg.com/profile_images/745743929753112587/czNblDjm.jpg" />
-//             <span className="card-title">Card Title</span>
-//           </div>
-//           <div className="card-content">
-//             <p>I am a very simple card. I am good at containing small bits of information.
-//             I am convenient because I require little markup to use effectively.</p>
-//           </div>
-//           <div className="card-action">
-//             <a href="#">This is a link</a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 const mapState = (state) => {
   return {
@@ -64,19 +16,6 @@ const mapState = (state) => {
   };
 };
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(fetchPastOrders());
-    },
-  };
-};
+const mapDispatch = null;
 
 export default connect(mapState, mapDispatch)(PreviousCarts);
-
-/**
- * PROP TYPES
- */
-PreviousCarts.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-};
